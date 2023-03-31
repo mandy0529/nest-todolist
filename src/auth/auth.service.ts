88e52348824c2e6257219709fbf00b51af871990
minji <321963741@naver.com>
2023-03-31 10:33:53 +0900

    signup logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,34 @@
 import { AuthDto } from './dto/auth.dto';
 import { PrismaService } from './../prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   // signup
-  signup(dto: AuthDto) {
-    return dto;
+  async signup(dto: AuthDto) {
+    //  password hashed
+    const hashPassword = await argon.hash(dto.password);
+
+    try {
+      //  create user to prisma
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hashPassword,
+        },
+      });
+
+      // password 빼기
+      delete user.password;
+      return user;
+    } catch (error) {
+      // 에러가 있을 경우 ForbiddenException으로 날려주기
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Credential is already taken');
+      }
+    }
   }
   // signin
   signin() {
