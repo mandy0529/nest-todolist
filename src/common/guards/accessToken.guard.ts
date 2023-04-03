6caa87600a1812d30ab19a6772180c59a1f4fd07
minji <321963741@naver.com>
2023-04-03 16:50:17 +0900

    accessTokenGuards logic

--- src/common/guards/accessToken.guard.ts
+++ src/common/guards/accessToken.guard.ts
@@ -1,7 +1,22 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
+@Injectable()
 export class AccessTokenGuard extends AuthGuard('jwt') {
-  constructor() {
+  constructor(private refector: Reflector) {
     super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    //
+    const isPublic = this.refector.getAllAndOverride('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) return true;
+
+    return super.canActivate(context);
   }
 }
