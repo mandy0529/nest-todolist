d920b7d5bef30ae9d057ba0d370237bae50529bb
minji <321963741@naver.com>
2023-04-03 16:18:56 +0900

    globl guard를 app.module에 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './common/guards';
 
 @Module({
   imports: [
@@ -10,6 +12,12 @@ import { ConfigModule } from '@nestjs/config';
     }),
     AuthModule,
     PrismaModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
+    },
   ],
 })
 export class AppModule {}
